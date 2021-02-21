a5eaf055c9037721769a42318a45f99d71ba8c89
Debojyotibabai <debojyotighosh060@gmail.com>
2021-02-21 11:50:00 +0530

    test

--- src/component/TopNews.js
+++ src/component/TopNews.js
@@ -145,7 +145,7 @@ const TopNews = () => {
       </div>
       {/* top news section */}
       <div className="news__section">
-        <h1 className="news__section__heading">Today's Top News</h1>
+        <h1 className="news__section__heading">Today's Top Newsss</h1>
 
         {/* carousel */}
         <Splide className="carousel">{allCarouselNews}</Splide>
