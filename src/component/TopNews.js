5c1d75d69dbd88be4b494a90f914d1aa7549b6b2
Debojyotibabai <debojyotighosh060@gmail.com>
2021-02-26 12:18:28 +0530

    nothing

--- src/component/TopNews.js
+++ src/component/TopNews.js
@@ -37,6 +37,9 @@ const TopNews = () => {
   // top news detail
   const [topNews, setTopNews] = useState([]);
 
+  // full news value
+  const [value, setValue] = useState(false);
+
   // sidebar news data fetch
   useEffect(() => {
     axios
